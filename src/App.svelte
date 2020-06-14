a7e91aa99b6645bfe3eb83e272784f45267baf43
unknown <alexos.droid@gmail.com>
2020-06-14 17:59:41 +0700

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,34 @@
+<script>
+  import Tailwindcss from "./Tailwindcss.svelte";
+
+  export let name;
+</script>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  h1 {
+    @apply bg-black text-4xl text-white;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
+
+<Tailwindcss />
+<main>
+  <h1>Hello {name}!</h1>
+  <p>
+    Visit the
+    <a href="https://svelte.dev/tutorial">Svelte tutorial</a>
+    to learn how to build Svelte apps.
+  </p>
+</main>
